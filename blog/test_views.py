34a549f7bd4b3a682b5c2551773ae16031a7ae6b
billbunkum <william.richard.hurst@gmail.com>
2016-09-19 11:14:46 -0400

    wiring up; blog/app

---
+++ blog/test_views.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+# Create your tests here.
+class BlogViewsTests(TestCase):
+
+		def test_views_index(self):
+			index_url = reverse('blog:index')
+
+			self.client.get(index_url)
+			self.assertEqual(response.status_code, 200)
